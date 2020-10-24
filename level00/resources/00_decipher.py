2d0be821952e29a33c3b72ffad3ec1e4271e45c1
LsHanaha <kirik193@yandex.ru>
2020-10-24 14:25:58 +0300

    add descriptions for 00-02

--- level00/resources/00_decipher.py
+++ level00/resources/00_decipher.py
@@ -1,5 +1,9 @@
 import sys
 
+if len(sys.argv) <= 1:
+	print("Подайте шифрованную строку как аргумент.")
+	exit()
+
 arg = [ord(c) - ord("a") for c in sys.argv[1]]
 for i in range(26):
 	var = [(c + i) % 26 + ord("a") for c in arg]
